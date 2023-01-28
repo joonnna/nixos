d3deffb2d86f9e4a88940673d22d2823532a3579
joonnna <jonfossmikalsen1@gmail.com>
2023-01-28 14:48:41 +0100

    change startip git symbol

--- hm/starship.nix
+++ hm/starship.nix
@@ -1,7 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.starship = {
-    enable = true;
+  programs.starship.enable = true;
+  programs.starship.settings = {
+    git_branch = {
+      symbol = " ";
+    };
   };
 }
