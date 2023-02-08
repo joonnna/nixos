cf862ac59ab1aca6110c006ec0b489ab16d7b0af
joonnna <jonfossmikalsen1@gmail.com>
2023-02-08 09:22:27 +0100

    fix bt setup

--- sound.nix
+++ sound.nix
@@ -1,8 +1,22 @@
 { config, libs, pkgs, ... }:
 
 {
-  hardware.bluetooth.enable = true;
-  hardware.pulseaudio.extraConfig = " load-module module-switch-on-connect ";
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+
+
+  sound.enable = false;
+  hardware.pulseaudio.extraConfig = "
+    load-module module-switch-on-connect
+    load-module module-bluetooth-policy
+    load-module module-bluetooth-discover
+  ";
 
   security.rtkit.enable = true;
   services.pipewire = {
