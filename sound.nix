7299e3e03cfafd793da01ef3959cd19daf21db69
joonnna <jonfossmikalsen1@gmail.com>
2023-08-26 17:01:03 +0200

    revert sound test

--- sound.nix
+++ sound.nix
@@ -24,18 +24,4 @@
     alsa.enable = true;
     pulse.enable = true;
   };
-
-  # source: https://nixos.wiki/wiki/PipeWire
-  # testing if this avoid crackling sound (interruption in audio output)
-  environment.etc = {
-    "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
-      context.properties = {
-        default.clock.rate = 48000
-        default.clock.quantum = 32
-        default.clock.min-quantum = 32
-        default.clock.max-quantum = 32
-      }
-    '';
-  };
 }
-
