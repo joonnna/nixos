9a54cf186a9368f4ec8ae510fdc17255f17b09b0
joonnna <jonfossmikalsen1@gmail.com>
2024-01-17 14:07:59 +0100

    use autorander for screens

--- work-desktop/settings.nix
+++ work-desktop/settings.nix
@@ -11,9 +11,6 @@
   boot.loader.grub.device = "nodev";
 
   services.xserver.displayManager.setupCommands = ''
-    LEFT='DP-2'
-    CENTER='DP-0'
-    RIGHT='DP-6'
-    ${pkgs.xorg.xrandr}/bin/xrandr --output $CENTER --primary --output $RIGHT --right-of $CENTER --output $LEFT --left-of $CENTER
+    ${pkgs.autorandr}/bin/autorandr horizontal
   '';
 }
