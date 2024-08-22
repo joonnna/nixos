d8ec1cc3b349b2c53fd128dfcbba0f6bc73839ea
joonnna <jonfossmikalsen1@gmail.com>
2024-08-22 14:27:22 +0200

    fix fzf keybinds

--- hm/nvim/config/lua/keybinds.lua
+++ hm/nvim/config/lua/keybinds.lua
@@ -43,20 +43,9 @@ set('n', '<leader>u', ':FzfLua command_history<CR>')
 set('n', '<leader>p', ':FzfLua search_history<CR>')
 set('n', '<leader>k', ':FzfLua keymaps<CR>')
 
-set('n', '<leader>g', ':LazyGit<CR>')
+set('n', '<leader>gg', ':LazyGit<CR>')
 set("n", "<leader>o", "<cmd>Oil --float<cr>", { desc = "Open file explorer" })
 
-
-local fzf_lua = require('fzf-lua')
-
-fzf_lua.setup({
-    keymap = {
-        builtin = {
-            ['<C-d>'] = 'preview-page-down',
-            ['<C-u>'] = 'preview-page-up',
-        },
-    },
-})
 
 -- Quickfix list
 set('n', '<C-j>', ':cnext<CR>zz')
--- hm/nvim/config/lua/plugins/fzf.lua
+++ hm/nvim/config/lua/plugins/fzf.lua
@@ -1,3 +1,12 @@
 require('fzf-lua').setup({
-    fzf_colors = true
+    fzf_colors = true,
+    keymap = {
+        builtin = {
+            ['<C-d>'] = 'preview-page-down',
+            ['<C-u>'] = 'preview-page-up',
+        },
+        fzf = {
+            ['ctrl-a'] = 'select-all+accept',
+        }
+    },
 })
