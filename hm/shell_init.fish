a1a583583f6d3cf8d8eb1a1ca50e42684a3a606e
joonnna <jonfossmikalsen1@gmail.com>
2024-10-08 14:41:46 +0200

    add ex fish function for unpacking stuff

--- hm/shell_init.fish
+++ hm/shell_init.fish
@@ -8,6 +8,41 @@ function fish_user_key_bindings
     bind p fish_clipboard_paste
 end
 
+
+function ex
+    if test -f "$argv[1]"
+        switch "$argv[1]"
+            case '*.tar.bz2'
+                tar xjf "$argv[1]"
+            case '*.tar.gz'
+                tar xzf "$argv[1]"
+            case '*.bz2'
+                bunzip2 "$argv[1]"
+            case '*.rar'
+                unrar x "$argv[1]"
+            case '*.gz'
+                gunzip "$argv[1]"
+            case '*.tar'
+                tar xf "$argv[1]"
+            case '*.tbz2'
+                tar xjf "$argv[1]"
+            case '*.tgz'
+                tar xzf "$argv[1]"
+            case '*.zip'
+                unzip "$argv[1]"
+            case '*.Z'
+                uncompress "$argv[1]"
+            case '*.7z'
+                7z x "$argv[1]"
+            case '*'
+                echo "'$argv[1]' cannot be extracted via ex()"
+        end
+    else
+        echo "'$argv[1]' is not a valid file"
+    end
+end
+
+
 set -U fish_greeting ""
 set -gx CARGO_HOME $HOME/.config/cargo
 set -x OP_BIOMETRIC_UNLOCK_ENABLED true
