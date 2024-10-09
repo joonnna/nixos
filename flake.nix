52928683ab3a6a7f45d7387fa1d23509187e5900
joonnna <jonfossmikalsen1@gmail.com>
2024-10-09 18:40:33 +0200

    split nvidia config based on gpu

--- flake.nix
+++ flake.nix
@@ -12,14 +12,14 @@
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       specialArgs = attrs;
-      modules = [ ./configuration.nix ./home-desktop/hardware-configuration.nix ./home-desktop/settings.nix ./nvidia.nix ];
+      modules = [ ./configuration.nix ./home-desktop/hardware-configuration.nix ./home-desktop/settings.nix ./nvidia-1080.nix ];
     };
 
     nixosConfigurations.jon-work-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       specialArgs = attrs;
-      modules = [ ./configuration.nix ./work-desktop/hardware-configuration.nix ./work-desktop/settings.nix ./github-runner.nix ./nvidia.nix ];
+      modules = [ ./configuration.nix ./work-desktop/hardware-configuration.nix ./work-desktop/settings.nix ./github-runner.nix ./nvidia-4090.nix ];
     };
 
     nixosConfigurations.jon-work-laptop = nixpkgs.lib.nixosSystem {
---
+++ nvidia-1080.nix
@@ -0,0 +1,30 @@
+{ config, home-manager, ... }:
+{
+  imports = [
+    home-manager.nixosModule
+  ];
+
+  services.xserver.videoDrivers = [ "nvidia-dkms" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    open = false;
+  };
+
+  home-manager.users.jon = { pkgs, lib, ... }: {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      settings = {
+        env = [
+          "LIBVA_DRIVER_NAME,nvidia"
+          "GBM_BACKEND,nvidia-drm"
+          "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+          "NVD_BACKEND,direct"
+          # "DRI_PRIME,pci-0000_01_00_0"
+          # "__VK_LAYER_NV_optimus,NVIDIA_only"
+        ];
+      };
+    };
+  };
+}
