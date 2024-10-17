8e453376e8f4632a62983844361c601701ee9607
joonnna <jonfossmikalsen1@gmail.com>
2024-10-17 15:13:22 +0200

    add nvidia settings in hopes of fixing HW acc in browser

--- nvidia-4090.nix
+++ nvidia-4090.nix
@@ -21,6 +21,7 @@
           "GBM_BACKEND,nvidia-drm"
           "__GLX_VENDOR_LIBRARY_NAME,nvidia"
           "NVD_BACKEND,direct"
+          "__NV_PRIME_RENDER_OFFLOAD,1"
           # "DRI_PRIME,pci-0000_01_00_0"
           # "__VK_LAYER_NV_optimus,NVIDIA_only"
         ];
