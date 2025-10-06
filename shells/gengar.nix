d306a0e6c0cb1beeeb3afb3258aa3027ea323c6a
joonnna <jonfossmikalsen1@gmail.com>
2025-10-06 10:17:59 +0200

    update gengar shell

--- shells/gengar.nix
+++ shells/gengar.nix
@@ -1,4 +1,6 @@
+
 { pkgs ? (import <nixpkgs> { config.allowUnfree = true; }) }:
+
 let
   python = pkgs.python311;
   pythonPackages = python.pkgs;
@@ -37,12 +39,20 @@ with pkgs; mkShell {
     pkg-config
     yasm
     fontconfig
-    # flamegraph
 
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+
+
+    # flamegraph
     # zlib
     # libGL
     # glib
-    #
+
     python311
     python311Packages.pip
   ];
@@ -54,6 +64,10 @@ with pkgs; mkShell {
     SOURCE_DATE_EPOCH=$(date +%s)
     VENV=.venv
 
+    # Need to expose python interpeter to pyo3 build script
+    export PYO3_PYTHON="$(which python)"
+    export PYTHON_SYS_EXECUTABLE="$(which python)"
+
     export LD_LIBRARY_PATH=/run/opengl-driver/lib:/run/opengl-driver-32/lib:$LD_LIBRARY_PATH;
     export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH;
 
@@ -63,7 +77,10 @@ with pkgs; mkShell {
 
     source ./$VENV/bin/activate
     export PYTHONPATH=`pwd`/$VENV/${python.sitePackages}/:$PYTHONPATH
+
+    # pip install --upgrade pip
     pip install -r src/yolo/python/requirements.txt
+    # pip install --no-build-isolation bytetracker
 
     export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
     export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
@@ -73,4 +90,5 @@ with pkgs; mkShell {
     ln -sf ${python.sitePackages}/* ./.venv/lib/python3.10/site-packages
   '';
 }
+
 
