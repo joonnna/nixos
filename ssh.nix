36809fc4a2543c26c4142532f70e37b4f87ece39
joonnna <jonfossmikalsen1@gmail.com>
2025-10-06 13:40:17 +0200

    add note on onpremise ssh/1password interation

--- ssh.nix
+++ ssh.nix
@@ -6,6 +6,10 @@
   # think this is due to keyring being locked when without this
   # enabled which I think unlocks the keyring on login
   # through lightdm (login prompt)
+  # For the onpremise work node a login needs to occur on the actual physical node to unlock
+  # the keyring through lightdm.
+  # If there only exists a SSH session 1password will be unable to unlock the keyring and
+  # will need a 2FA code.
   security.pam.services.lightdm.enableGnomeKeyring = true;
 
   services.openssh = {
