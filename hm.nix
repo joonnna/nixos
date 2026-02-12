664a213c206e6a183b86ba40c325866b1a317d7f
joonnna <jonfossmikalsen1@gmail.com>
2026-02-12 14:26:22 +0100

    update rust version in shell

--- hm.nix
+++ hm.nix
@@ -188,7 +188,7 @@
     home.file."workspace/rust-toolchain.toml".text = ''
       [toolchain]
       profile = "default"
-      channel = "1.91.1"
+      channel = "1.93.0"
       components = ["rust-analyzer"]
     '';
 
